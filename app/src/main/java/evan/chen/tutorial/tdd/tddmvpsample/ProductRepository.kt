8372d7083427c128f87df0bda9944691fd9b6c2a
Evan <pifan76@gmail.com>
2019-10-10 22:54:17 +0800

    通過repository的測試

--- app/src/main/java/evan/chen/tutorial/tdd/tddmvpsample/ProductRepository.kt
+++ app/src/main/java/evan/chen/tutorial/tdd/tddmvpsample/ProductRepository.kt
@@ -2,10 +2,11 @@ package evan.chen.tutorial.tdd.tddmvpsample
 
 import evan.chen.tutorial.tdd.tddmvpsample.api.IProductAPI
 
-class ProductRepository(productAPI: IProductAPI) : IProductRepository {
+class ProductRepository(val productAPI: IProductAPI) : IProductRepository {
     override fun getProduct(productId: String, callback: IProductRepository.LoadProductCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        productAPI.getProduct(productId, object : IProductAPI.LoadAPICallback{
 
+        })
+    }
 
 }
