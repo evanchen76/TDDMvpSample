7663d7cc72f15f18a0b1f200423a6488a5c706fd
Evan <pifan76@gmail.com>
2019-10-10 22:44:37 +0800

    通過Presenter測試

--- app/src/main/java/evan/chen/tutorial/tdd/tddmvpsample/ProductPresenter.kt
+++ app/src/main/java/evan/chen/tutorial/tdd/tddmvpsample/ProductPresenter.kt
@@ -1,9 +1,10 @@
 package evan.chen.tutorial.tdd.tddmvpsample
 
-class ProductPresenter(repository: IProductRepository) : ProductContract.IProductPresenter
+class ProductPresenter(val repository: IProductRepository) : ProductContract.IProductPresenter
 {
     override fun getProduct(productId: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        repository.getProduct(productId, object : IProductRepository.LoadProductCallback{
+            //還沒處理View的callback
+        })
     }
-
 }
