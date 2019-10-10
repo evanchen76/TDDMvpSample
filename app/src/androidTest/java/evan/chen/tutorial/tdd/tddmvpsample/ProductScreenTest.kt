2b1ec2340020ef14e47369a1916421e088196026
Evan <pifan76@gmail.com>
2019-10-10 22:24:45 +0800

    init 增加activity_product.xml、ProductActivity

---
+++ app/src/androidTest/java/evan/chen/tutorial/tdd/tddmvpsample/ProductScreenTest.kt
@@ -0,0 +1,27 @@
+package evan.chen.tutorial.tdd.tddmvpsample
+
+import android.content.Intent
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
+import android.support.test.filters.LargeTest
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ProductScreenTest {
+    
+}
---
+++ app/src/main/java/evan/chen/tutorial/tdd/tddmvpsample/ProductActivity.kt
@@ -0,0 +1,13 @@
+package evan.chen.tutorial.tdd.tddmvpsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+
+    }
+}
